b9088f06b419d3cef3d69e1863e5f95442a69692
Dmitriy Polskoy <polskoy@gmail.com>
2016-01-18 20:43:55 -0800

    UI polishing and Outlet creating

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var playBtn: UIButton!
+	@IBOutlet weak var logoImg: UIImageView!
+	@IBOutlet weak var multipleTxt: UITextField!
+	@IBOutlet weak var addBtn: UIButton!
+	@IBOutlet weak var infoLbl: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		infoLbl.adjustsFontSizeToFitWidth = true
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -20,6 +28,10 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func onPlayPressed(sender: UIButton) {
+	}
 
+	@IBAction func onAddPressed(sender: UIButton) {
+	}
 }
 
